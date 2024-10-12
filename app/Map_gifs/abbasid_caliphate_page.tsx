97e8b73f7a77f262c7bc742295d0caf32a0c13a7
Dylan Owens <dylowens@gmail.com>
2024-10-12 16:22:56 -0700

    commit

---
+++ app/Map_gifs/abbasid_caliphate_page.tsx
@@ -0,0 +1,13 @@
+export default function AbbasidCaliphatePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <div className="w-full max-w-md bg-white rounded-lg shadow-lg overflow-hidden">
+        <img 
+          src="/images/Abbasid_Caliphate_Gif.gif" 
+          alt="Abbasid Caliphate Map"
+          className="w-full h-auto"
+        />
+      </div>
+    </div>
+  );
+}
